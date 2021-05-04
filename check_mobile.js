d285202d5d2ac5208ef3ef43f39ba8ba4b45d4ef
namumetro <71622706+namumetro@users.noreply.github.com>
2021-05-04 11:31:12 +0900

    Create check_mobile.js

---
+++ check_mobile.js
@@ -0,0 +1,6 @@
+function isMobile() {
+    return /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i.test(navigator.userAgent);
+}
+if(isMobile()){
+    alert("모바일 모드에서는 UI가 깨질 수 있습니다. 데스크톱 버전으로 보시는 것을 추천드립니다.")
+}
